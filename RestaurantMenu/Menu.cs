b410d0d9de0edd3ffadfcf54115fe601e3f358a1
Happy <biswasnedhi09@gmail.com>
2022-08-17 19:40:55 -0500

    Add project files.

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu
+{
+    public class Menu
+    {
+       public List<MenuItem> MenuItems { get; set; }
+        public DateTime Updated { get; set; }
+        public Menu(List<MenuItem> menuItems, DateTime updated)
+        {
+            MenuItems = menuItems;
+            Updated = updated;
+        }
+    }
+}
